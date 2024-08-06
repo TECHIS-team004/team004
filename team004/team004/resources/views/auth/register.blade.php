4e42181cdaa7a2c0f6d4c3a9345d91e8d17955b1
m-s-0221 <maringo.1207@gmail.com>
2024-08-06 11:13:55 +0900

    ログイン・登録画面　完了

--- team004/team004/resources/views/auth/register.blade.php
+++ team004/team004/resources/views/auth/register.blade.php
@@ -24,6 +24,17 @@
             <form method="POST" action="{{ route('register') }}">
                 @csrf
                 <div class="row mb-3">
+                    <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('お名前') }}</label>
+                    <div class="col-md-6">
+                        <input id="name" type="name" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name">
+                        @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+                <div class="row mb-3">
                     <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('メールアドレス') }}</label>
                     <div class="col-md-6">
                         <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
