8588ca41c862fef1b370d85d53a23bdf75c01d4b
superwata <84887179+superwata@users.noreply.github.com>
2024-08-09 13:09:46 +0900

    渡辺　ホーム画面修正

--- team004/team004/resources/views/home.blade.php
+++ team004/team004/resources/views/home.blade.php
@@ -1,25 +1,15 @@
-<!-- resources/views/home.blade.php -->
-<!DOCTYPE html>
-<html>
-<head>
-    <title>商品管理システム</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-</head>
-<body>
-    <div class="container">
-        @include('side')
+@extends('layouts.app')
+
+@section('content')
         <div class="content">
-            <div class="header">
+            <!-- <div class="header">
                 <div class="header-buttons">
-                    <a href="{{ url('/register') }}">アカウント登録画面</a>
                     <a href="{{ url('/login') }}">ログイン画面</a>
                     <a href="{{ url('/logout') }}">ログアウト</a>
                 </div>
-            </div>
+            </div> -->
             <div class="main-content">
                 <h2>ホーム画面へようこそ(team004)</h2>
             </div>
         </div>
-    </div>
-</body>
-</html>
+@endsection
