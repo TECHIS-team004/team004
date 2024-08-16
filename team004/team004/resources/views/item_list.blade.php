c6c4cc7027765e09e3a57d9c01482fd65ce7103c
yamatakezo1111 <yamatakezo1111@gmail.com>
2024-08-16 14:27:21 +0900

    ページネーション削除・一般ユーザーの場合サイドバーにユーザー一覧非表示

--- team004/team004/resources/views/item_list.blade.php
+++ team004/team004/resources/views/item_list.blade.php
@@ -51,6 +51,6 @@
                 </tbody>
             </table>
 
-            {{ $items->links() }}
+           <!-- {{ $items->links() }} -->
             </div>
 @endsection
--- team004/team004/resources/views/side.blade.php
+++ team004/team004/resources/views/side.blade.php
@@ -3,7 +3,10 @@
             <h4>商品管理システム</h4>
             <ul>
                 <li><a href="{{ url('/item_list') }}">商品一覧</a></li>
+
+            @if(auth()->check() && auth()->user()->is_admin)
                 <li><a href="{{ url('/users') }}">ユーザー一覧</a></li>
+            @endif
             </ul>
         </div>
         <div class="sidebar-logo">
